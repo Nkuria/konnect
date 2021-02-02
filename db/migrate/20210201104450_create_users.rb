c745cd61b3f054d252c788b3317a10a3d5e59a52
Nick Kuria <nkuria01@gmail.com>
2021-02-02 16:38:18 +0300

    Ran user migration

--- db/migrate/20210201104450_create_users.rb
+++ db/migrate/20210201104450_create_users.rb
@@ -1,7 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.
+      t.string :Username
+      t.string :Fullname
+      t.string :Photo
+      t.string :Coverimage
+
 
       t.timestamps
     end
