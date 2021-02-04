817b354c6b17823aaac964307ef82c9e557a434b
Nick Kuria <nkuria01@gmail.com>
2021-02-04 08:40:10 +0300

    Added user and post associations on models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
     attr_accessible :content
+    belongs_to :user
+
     validates :user_id, presence: true
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
     mount_uploader :Photo, ImageUploader
     mount_uploader :Coverimage, ImageUploader
+    has_many :posts
+
+
     validates_uniqueness_of :Username
     validates :Username, presence: true
     validates :Fullname, presence: true
