94101d941d1ad163a6a1ce2c96877d96fc678a8e
Nick Kuria <nkuria01@gmail.com>
2021-02-04 08:30:08 +0300

    Created post model and ran migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ApplicationRecord
     mount_uploader :Photo, ImageUploader
     mount_uploader :Coverimage, ImageUploader
     validates_uniqueness_of :Username
-    validates_uniqueness_of :email
+    validates :Username, presence: true
+    validates :Fullname, presence: true
+    validates :Photo, presence: true
+    validates :Coverimage, presence: true
+    
 end
