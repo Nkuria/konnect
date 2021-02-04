704a15939ea42deea653e96c56676da7c7906fb4
Nick Kuria <nkuria01@gmail.com>
2021-02-04 18:28:03 +0300

    Worked on posts controller and created partial for new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,13 @@
 class PostsController < ApplicationController
+    def create
+        @post = current_user.posts.build(params[:posts])
+        if @post.save
+            Flash[:Succes] = "Post succesfully created"
+        else
+            puts errors.full_messages
+        end
+    end
+    def destroy
+        
+    end
 end
